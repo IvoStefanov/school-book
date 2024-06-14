d3434d96f5398f823aed3a754b791b4e8a34ef10
IlinaGergova <ili.gergova@gmail.com>
2024-06-14 22:05:32 +0300

    return user data and token at once

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,23 +3,34 @@ import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { User } from './users/user.entity';
+
+interface UserData {
+  user: User;
+  token: string;
+}
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private authService: AuthService
+    private authService: AuthService,
   ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    const userToken = this.authService.login(req.user);
+    const user: UserData = {
+      user: req.user,
+      token: (await userToken).access_token,
+    };
+    return user;
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Get('profile')
+  // getProfile(@Request() req) {
+  //   return req.user;
+  // }
 }
