fe9fb62fac4167042e3c13ebc2fc17a39d55213f
IlinaGergova <ili.gergova@gmail.com>
2024-07-01 21:37:21 +0300

    create school

--- src/schools/schools.controller.ts
+++ src/schools/schools.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { SchoolsService } from './schools.service';
+import { DataSource } from 'typeorm';
 
 @Controller()
 export class SchoolsController {
-  constructor(private schoolsService: SchoolsService) {}
+  constructor(
+    private schoolsService: SchoolsService,
+    private dataSource: DataSource,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Get('schools')
   async getSchools() {
     return this.schoolsService.findAll();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('create-school')
+  async createSchool(@Request() req) {
+    return this.schoolsService.create(
+      req.name,
+      req.address,
+      req.contact,
+      this.dataSource,
+    );
   }
 }
